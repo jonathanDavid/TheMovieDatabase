801b181d47aa4dc61d1e2035dfaac1e21d2d7205
johan <jdavid.ilias@gmail.com>
2018-05-03 11:14:46 -0500

    Primera consulta
    Se consulto en la api la primera pagina y se extrajo del archivo json el numeor de la pagina y el titulo de cada pelicula

--- TheMovieDB/routes/index.js
+++ TheMovieDB/routes/index.js
@@ -1,9 +1,46 @@
-let express = require('express');
-let router = express.Router();
+const express = require('express');
+const router = express.Router();
+const request = require('request');
 
-/* GET home page. */
+/* GET home page. 
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});*/
+
+const api_key = "b29ba2335d06cf242272f5d0955bcffb";
+
+const options = {
+    method: 'GET',
+    uri: `https://api.themoviedb.org/3/movie/popular?api_key=${api_key}&language=es-CO&page=1`,
+    headers: {
+        'Content-Type': 'application/json'
+    }
+}
+
+function consume() {
+    return new Promise((resolve, reject) => {
+        request(options, (err, res,  body) => {
+            if (err) {
+                reject(err);
+            }else{
+            	resolve(body);
+            }
+        });
+    });
+}
+
+
+router.get('/', function(req,res){
+    consume().then((data)=>{
+    	let DataJ = JSON.parse(data);
+    	//res.write(data);
+    	res.render('index', { title: 'The Movie DB', pageD: DataJ.page, movies: DataJ.results});
+    }).catch((err)=>{
+        res.write('Error al cargar los datos.');
+    }),() => {
+        res.end();
+    };
 });
+
 
 module.exports = router;
